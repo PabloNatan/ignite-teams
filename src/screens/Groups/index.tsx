6ddf6ea25418de96bbe2173623573ad7a771ebee
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2022-10-20 08:57:14 -0300

    feat: add styled-components, path mapping and first commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,9 @@
+import * as S from "./styles";
+
+export function Groups() {
+  return (
+    <S.Wrapper>
+      <S.CustomText>Groups</S.CustomText>
+    </S.Wrapper>
+  );
+}
---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import styled from "styled-components/native";
+
+export const Wrapper = styled(View)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #333;
+`;
+
+export const CustomText = styled(Text)`
+  font-size: 16px;
+  color: #fff;
+`;
