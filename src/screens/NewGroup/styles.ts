ca494099e51e1b8d9a1ee8ee4a0b4b39957cdfe0
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2022-10-25 07:49:26 -0300

    fix: add safeAreaView on screens

--- src/screens/NewGroup/styles.ts
+++ src/screens/NewGroup/styles.ts
@@ -1,8 +1,9 @@
-import { View, Text } from "react-native";
-import styled from "styled-components/native";
 import { UsersThree } from "phosphor-react-native";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import styled from "styled-components/native";
 
-export const Wrapper = styled(View)`
+export const Wrapper = styled(SafeAreaView)`
   flex: 1;
   padding: 24px;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,7 +1,7 @@
-import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import styled, { css } from "styled-components/native";
 
-export const Wrapper = styled(View)`
+export const Wrapper = styled(SafeAreaView)`
   flex: 1;
 
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
